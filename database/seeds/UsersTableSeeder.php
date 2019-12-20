707880b5c3efaa62b4d266eb75848330993e1c43
Juan Gamonal H <juangamonalh@gmail.com>
2019-12-20 01:04:31 -0300

    seeders y cosas

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
-// use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        // User::create(['' => 'juan@juntapp.cl']);
+        $users = [
+            [
+                'name' => 'admin',
+                'email' => 'admin@juntapp.cl',
+                'password' => Hash::make('12345678'),
+                'admin' => 1
+            ]
+        ];
+
+        foreach ($users as $user) {
+            DB::table('users')
+                ->insert($user);
+        }
     }
 }
